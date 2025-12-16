465be5e1ed32955fb9ccfede3885bf66b3688371
Lorena Turansche <lorena.turansche93@gmail.com>
2025-12-16 14:40:33 +0200

    ProbaController creat

---
+++ Controllers/ProbaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Turansche_Lorena_Lab1.Controllers
+{
+    public class ProbaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
